92d9a0f9f73bd576c753cc45e4734a627cc52cbc
mrBidone <ab.bz@ukr.net>
2024-08-02 23:29:03 +0200

    Add some components

--- src/App.jsx
+++ src/App.jsx
@@ -1,64 +1,27 @@
-import Bar from "./components/Bar/Bar";
-import Button from "./components/Button/Button";
-import ClickCounter from "./components/ClickCounter/ClickCounter";
-import ClicksButton from "./components/ClicksButton/ClicksButton";
-import CustomButton from "./components/CustomButton/CustomButton";
-import { useState } from "react";
+import LangSwitcher from "./components/LangSwitcher/LangSwitcher";
+import LoginForm from "./components/LoginForm/LoginForm";
+import SearchBar from "./components/SearchBar/SearchBar";
 import Section from "./components/Section/Section";
-import Modal from "./components/Modal/Modal";
+import { useState } from "react";
 
 function App() {
-  const [bottles, setBottles] = useState({
-    beer: 0,
-    wine: 0,
-    whiskey: 0,
-  });
-
-  const onBarSupplyAdd = (alcoName) => {
-    setBottles({ ...bottles, [alcoName]: bottles[alcoName] + 1 });
-
-    // if (alcoName === "beer") {
-    //   setBottles({ ...bottles, beer: bottles["beer"] + 1 });
-    // }
-    // if (alcoName === "wine") {
-    //   setBottles({ ...bottles, wine: bottles["wine"] + 1 });
-    // }
-    // if (alcoName === "whiskey") {
-    //   setBottles({ ...bottles, whiskey: bottles["whiskey"] + 1 });
-    // }
+  const handleLogin = (userData) => {
+    console.log(userData);
   };
 
-  const total = bottles.beer + bottles.wine + bottles.whiskey;
-
-  const [isModalOpen, setIsModalOpen] = useState(false);
-  const onOpenModal = () => {
-    setIsModalOpen(true);
-  };
-  const onCloseModal = () => {
-    setIsModalOpen(false);
-  };
+  const [lang, setLang] = useState("uk");
   return (
     <>
-      <Section title="Section Bar">
-        <Bar
-          beer={bottles.beer}
-          wine={bottles.wine}
-          whiskey={bottles.whiskey}
-          total={total}
-          onBarSupplyAdd={onBarSupplyAdd}
-        />
+      <Section>
+        <h1>Please login to your account!</h1>
+        <LoginForm onLogin={handleLogin} />
       </Section>
-      <Button />
-      <CustomButton message="Playing music!">Play</CustomButton>
-      <CustomButton message="Uploading your data">Upload</CustomButton>
-      <ClicksButton />
-      <ClickCounter />
-
-      <Section title={"Modal section"}>
-        <button onClick={() => setIsModalOpen(!isModalOpen)}>
-          {isModalOpen ? "Stop" : "Start"}
-        </button>
-        {isModalOpen && <Modal onCloseModal={onCloseModal} />}
+      <Section title={"Seach Bar"}>
+        <SearchBar />
+      </Section>
+      <Section title={"Select"}>
+        <p>Selected language: {lang}</p>
+        <LangSwitcher value={lang} onSelect={setLang} />
       </Section>
     </>
   );
---
+++ src/components/LangSwitcher/LangSwitcher.jsx
@@ -0,0 +1,24 @@
+import { useId } from "react";
+
+const LangSwitcher = ({ value, onSelect }) => {
+  const selectId = useId();
+
+  return (
+    <div>
+      <label htmlFor={selectId}>Choose Language : </label>
+      <select
+        id={selectId}
+        value={value}
+        onChange={(e) => {
+          onSelect(e.target.value);
+        }}
+      >
+        <option value="🇬🇧">English</option>
+        <option value="🇺🇦">Ukrainian</option>
+        <option value="🇵🇱">Polish</option>
+      </select>
+    </div>
+  );
+};
+
+export default LangSwitcher;
